e694ab2cecd2b1f27483599323e6a4ca9bc4d13b
brittanymae01 <brittanymae01>
2019-11-05 14:10:43 -0800

    some styling

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -3,7 +3,7 @@ import React from "react";
 const OperatorButton = (props) => {
   return (
     <>
-      <button>{props.button.char}</button>
+      <button className='button'>{props.button.char}</button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </>
   );
